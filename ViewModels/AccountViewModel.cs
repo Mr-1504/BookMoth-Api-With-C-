01e9c61a1584d4b70491aad92f1da9bad844fa72
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-01 21:37:41 +0700

    Create AccountViewModel.cs

---
+++ ViewModels/AccountViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace BookMoth_Api_With_C_.ViewModels
+{
+    public class AccountViewModel
+    {
+        public int AccountId { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public int? AccountType { get; set; }
+    }
+}
