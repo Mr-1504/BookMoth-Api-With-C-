85d36eee4cb002179dcdcda5ed8fda5297c58c20
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-17 17:19:22 +0700

    convert identifier from string to bool

--- RequestModels/RegisterRequest.cs
+++ RequestModels/RegisterRequest.cs
@@ -12,8 +12,9 @@
 
         public string? Coverphoto { get; set; }
 
-        public string? Identifier { get; set; }
+        public bool? Identifier { get; set; }
         public int? Gender { get; set; }
+        public string DateOfBirth { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
         public int AccountType { get; set; }
