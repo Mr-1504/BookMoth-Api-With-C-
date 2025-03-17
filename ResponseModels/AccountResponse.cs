c414568286d756dd4bbacea2a640c68d3e91fd30
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-17 17:19:29 +0700

    Create AccountResponse.cs

---
+++ ResponseModels/AccountResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace BookMoth_Api_With_C_.ResponseModels
+{
+    public class AccountResponse
+    {
+        public int AccountId { get; set; }
+
+        public string Email { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public int? AccountType { get; set; }
+    }
+}
