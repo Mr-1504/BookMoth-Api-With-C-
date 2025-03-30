9e8cb286dae245f00a7b29b0bdd66099385365d9
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-30 21:24:50 +0700

    Update AuthorWallet.cs

--- Models/AuthorWallet.cs
+++ Models/AuthorWallet.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace BookMoth_Api_With_C_.Models;
+﻿namespace BookMoth_Api_With_C_.Models;
 
 public partial class AuthorWallet
 {
@@ -12,6 +9,4 @@ public partial class AuthorWallet
     public decimal? AccumulatedBalance { get; set; }
 
     public string? PaymentInfo { get; set; }
-
-    public virtual ICollection<PaymentInvoice> PaymentInvoices { get; set; } = new List<PaymentInvoice>();
 }
