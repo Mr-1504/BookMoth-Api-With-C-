ccd92b05a976c13ce830f4be7670e1033d718ff1
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-30 21:25:26 +0700

    Update CreateOrderRequest.cs

--- RequestModels/CreateOrderRequest.cs
+++ RequestModels/CreateOrderRequest.cs
@@ -1,9 +1,11 @@
-﻿namespace BookMoth_Api_With_C_.RequestModels
+﻿using static BookMoth_Api_With_C_.Models.Enums;
+
+namespace BookMoth_Api_With_C_.RequestModels
 {
     public class CreateOrderRequest
     {
         public long Amount { get; set; }
-        public bool TransactionType { get; set; }
+        public TransactionType TransactionType { get; set; }
         public string Description { get; set; }
     }
 }
