ec8154133ee7cfcfd75006e8523faa268738ea42
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-30 21:24:57 +0700

    Create Enums.cs

---
+++ Models/Enums.cs
@@ -0,0 +1,20 @@
+﻿namespace BookMoth_Api_With_C_.Models
+{
+    public class Enums
+    {
+        public enum TransactionType
+        {
+            Deposit = 1,
+            Withdrawal = 2,
+            Transfer = 3,
+            Payment = 4,
+        }
+
+        public enum PaymentMethod
+        {
+            ZaloPay = 1,
+            Wallet = 2
+        }
+
+    }
+}
