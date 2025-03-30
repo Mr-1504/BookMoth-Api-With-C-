388d370c2470311439759f18b15b85bd2585ef98
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-30 21:25:15 +0700

    Update Transactions.cs

--- Models/Transactions.cs
+++ Models/Transactions.cs
@@ -1,11 +1,15 @@
-﻿namespace BookMoth_Api_With_C_.Models
+﻿using static BookMoth_Api_With_C_.Models.Enums;
+
+namespace BookMoth_Api_With_C_.Models
 {
     public class Transactions
     {
         public required string TransactionId { get; set; }
-        public int WalletId { get; set; }
+        public int? SenderWalletId { get; set; }
+        public int? ReceiverWalletId { get; set; }
+        public PaymentMethod Payment_Method_Id { get; set; }
         public decimal Amount { get; set; }
-        public bool TransactionType { get; set; }
+        public TransactionType TransactionType { get; set; }
         public int Status { get; set; }
         public required string Description { get; set; }
         public DateTime Created_At { get; set; }
