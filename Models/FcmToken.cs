3512389f162b7fde096cd2380481ebcdfe83cd43
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-31 10:30:54 +0700

    Create FcmToken.cs

---
+++ Models/FcmToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookMoth_Api_With_C_.Models;
+
+public partial class FcmToken
+{
+    public int Id { get; set; }
+
+    public int? AccountId { get; set; }
+
+    public string DeviceId { get; set; } = null!;
+
+    public string Token { get; set; } = null!;
+
+    public virtual Account? Account { get; set; }
+}
