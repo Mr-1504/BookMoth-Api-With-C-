c9a9c3ac56c2e06d2e3297a8dd00f06330a35a68
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-31 17:38:17 +0700

    Create OrderRequest.cs

---
+++ RequestModels/OrderRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BookMoth_Api_With_C_.RequestModels
+{
+    public class OrderRequest
+    {
+        public int WorkId { get; set; }
+        public string OrderTime { get; set; }
+        public string Mac { get; set; }
+    }
+}
