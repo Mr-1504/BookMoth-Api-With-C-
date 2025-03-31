bac9a754f1a3cfba9284e1eb75e178b78a529020
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-31 10:31:00 +0700

    Create Follow.cs

---
+++ Models/Follow.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookMoth_Api_With_C_.Models;
+
+public partial class Follow
+{
+    public int FollowerId { get; set; }
+
+    public int FollowingId { get; set; }
+
+    public virtual Profile Following { get; set; } = null!;
+}
