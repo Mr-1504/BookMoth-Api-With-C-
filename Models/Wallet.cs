d22a98669a7e6b521f0e06eb1611d0af0182a4e2
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-31 10:31:32 +0700

    Update Wallet.cs

--- Models/Wallet.cs
+++ Models/Wallet.cs
@@ -10,12 +10,16 @@ public partial class Wallet
     public int AccountId { get; set; }
 
     public decimal Balance { get; set; }
-    public required string HashedPin { get; set; }
-    public required string Salt { get; set; }
+
+    public string Hashedpin { get; set; } = null!;
+
+    public string Salt { get; set; } = null!;
 
     public int Status { get; set; }
 
     public virtual Account Account { get; set; } = null!;
 
-    public virtual ICollection<Iachistory> Iachistories { get; set; } = new List<Iachistory>();
+    public virtual ICollection<Iachistory> IachistoryReceiverWallets { get; set; } = new List<Iachistory>();
+
+    public virtual ICollection<Iachistory> IachistorySenderWallets { get; set; } = new List<Iachistory>();
 }
