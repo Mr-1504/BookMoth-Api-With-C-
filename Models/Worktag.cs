049df007f1ca70441d50d41a2fb0e8157b785aa0
Mr-1504 <truongvan.minh1504@gmail.com>
2025-03-31 10:31:40 +0700

    Create Worktag.cs

---
+++ Models/Worktag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BookMoth_Api_With_C_.Models;
+
+public partial class Worktag
+{
+    public int WorkId { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+
+    public virtual Work Work { get; set; } = null!;
+}
