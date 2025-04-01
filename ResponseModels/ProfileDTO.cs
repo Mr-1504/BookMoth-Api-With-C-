b0b029d2f73bce1bface18944e202985ae68f8f8
Mr-1504 <truongvan.minh1504@gmail.com>
2025-04-01 23:46:35 +0700

    Create ProfileDTO.cs

---
+++ ResponseModels/ProfileDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace BookMoth_Api_With_C_.ResponseModels
+{
+    public class ProfileDTO
+    {
+        public int Profile_Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string? First_Name { get; set; }
+        public string? Last_Name { get; set; }
+        public string? Avatar { get; set; }
+        public int MutualCount { get; set; }
+    }
+}
